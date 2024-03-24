0de27afb3f8bda9d5ab0b898bb16c8a632544e35
NjuLSL <522023320078@qq.com>
2024-03-24 19:38:37 +0800

    commit

---
+++ src/main/java/com/njulsl/videosys/VideosysApplication.java
@@ -0,0 +1,13 @@
+package com.njulsl.videosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideosysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideosysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/njulsl/videosys/VideosysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njulsl.videosys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideosysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
